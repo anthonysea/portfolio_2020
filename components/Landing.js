84c0348b84ce299a3e15579547a004f468baaf18
anthonysea <anthonyqchao@gmail.com>
2020-05-25 03:40:56 -0700

    Using CSS modules

--- components/Landing.js
+++ components/Landing.js
@@ -1,26 +1,17 @@
-
+import styles from './Landing.module.css';
 
 export default function Landing({ children, props }) {
 
-    const scrollTo = (section) => {
-        if (typeof window !== "undefined") {
-            console.log(window)
-            window.scrollTo(section)
-        }
-    }
-
     return (
-        <div className="gradient min-h-screen flex flex-col text-center justify-center">
-            <div className="top-banner">
-                <h1 className="text-5xl font-semibold">Anthony Chao</h1>
-                <p>Welcome to my website :~)</p>
-                <div className="my-4 flex flex-row justify-center">
-                    {/* <span className="mx-2 px-2" onClick={() => scrollTo('#about')}>About</span> */}
-                    <div className="mx-2 px-2"><a href="#about">About</a></div>
-                    <div className="mx-2 px-2"><a href="#skills">Skills</a></div>
-                    <div className="mx-2 px-2"><a href="#projects">Projects</a></div>
-                    <div className="mx-2 px-2"><a href="#contact">Contact</a></div>
-                </div>
+        <div className={ styles.landing }>
+            <h1 className={ styles.heading }>Anthony Chao</h1>
+            <p>Welcome to my website :~)</p>
+            <div className="my-4 flex flex-row justify-center">
+                {/* <span className="mx-2 px-2" onClick={() => scrollTo('#about')}>About</span> */}
+                <div className="mx-2 px-2"><a href="#about">About</a></div>
+                <div className="mx-2 px-2"><a href="#skills">Skills</a></div>
+                <div className="mx-2 px-2"><a href="#projects">Projects</a></div>
+                <div className="mx-2 px-2"><a href="#contact">Contact</a></div>
             </div>
         </div>
     )
