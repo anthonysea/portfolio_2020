c1fa3e1c83742bdef7dd76b8e8761a76993ec986
anthonysea <anthonyqchao@gmail.com>
2021-07-23 00:18:35 -0700

    testing

--- components/Resume.js
+++ components/Resume.js
@@ -3,6 +3,7 @@ import SectionHeading from "./SectionHeading";
 export default function Resume() {
     return (
         <div id="resume" className="mb-4">
+            {/* This is a comment i'm adding */}
             <SectionHeading title="resume" imgUrl="images/resume.png" />
             <div className="p-2">
                 <p>View my resume <a className="text-blue-400 hover:text-blue-600 transition-colors duration-300" href="anthony_chao_resume.pdf">here</a>!</p>
